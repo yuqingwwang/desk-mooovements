a15b76605612bf0908876e1562bef316b183c3a0
ShaughnAnderson94 <andersji94@gmail.com>
2023-11-07 17:29:51 +0000

    homepage caorosiles
    Co-authored-by: JamesESS <JamesESS@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { SupabaseCall } from '@/utils/supabaseCall';
+import Carousel from './components/Carousel';
 
 type City = {
   id: number;
@@ -24,18 +25,26 @@ type Place = {
 };
 
 export default async function Home() {
+  
   let cities: City[] | null = null;
   let places: Place[] | null = null;
-  cities = await SupabaseCall('cities', 'id,name,country');
+  cities = await SupabaseCall('cities', 'id,name,country, work_spaces(count)');
 
   places = await SupabaseCall('work_spaces', 'id,name,address,image,city');
   return (
-
     <main>
       <h1>Welcome to desk-mooovements!</h1>
       {/* {fetchError && <p>{fetchError}</p>} */}
-      {cities?.map((city) => <p key={city.name}>{city.name}</p>)}
-      {places?.map((place) => <p key={place.name}>{place.name}</p>)}
+      <div
+        id='popularCities'
+        className='border-4 border-double border-yellow-500 flex justify-between flex-wrap'
+      >
+        <Carousel places = {places}/>
+        <Carousel cities = {cities} />
+
+       
+        </div>
     </main>
   );
 }
+
