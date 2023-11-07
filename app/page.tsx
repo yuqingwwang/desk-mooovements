eca7cb4ec010938a664808651a0e511ea7795ab1
JamesESS <jesandfordsmith@gmail.com>
2023-11-07 17:58:00 +0000

    husky test

--- app/page.tsx
+++ app/page.tsx
@@ -26,11 +26,15 @@ type Place = {
 export default async function Home() {
   let cities: City[] | null = null;
   let places: Place[] | null = null;
-  cities = await SupabaseCall('cities', 'id,name,country','',"");
+  cities = await SupabaseCall('cities', 'id,name,country', '', '');
 
-  places = await SupabaseCall('work_spaces', 'id,name,address,image,city',"","");
+  places = await SupabaseCall(
+    'work_spaces',
+    'id,name,address,image,city',
+    '',
+    ''
+  );
   return (
-
     <main>
       <h1>Welcome to desk-mooovements!</h1>
       {/* {fetchError && <p>{fetchError}</p>} */}
