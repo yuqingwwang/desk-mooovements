0741a9ecf936cd4350872bbc1c6c6c6954bec7bb
Yuqing Wang <44486576+yuqingwwang@users.noreply.github.com>
2023-11-11 16:34:52 +0000

    corrected path

--- app/middleware.ts
+++ app/middleware.ts
@@ -2,7 +2,7 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 import { NextResponse } from 'next/server';
 
 import type { NextRequest } from 'next/server';
-import type { Database } from './lib/supabase';
+import type { Database } from '../database.types';
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
