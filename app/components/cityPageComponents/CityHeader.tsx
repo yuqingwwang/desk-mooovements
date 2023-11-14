598afb03ba389c33a6f53958d802e756ccde9dc7
JamesESS <jesandfordsmith@gmail.com>
2023-11-14 11:48:44 +0000

    resize heading

--- app/components/cityPageComponents/CityHeader.tsx
+++ app/components/cityPageComponents/CityHeader.tsx
@@ -3,13 +3,13 @@ import { Heading, Text, Box } from '@radix-ui/themes';
 export default function CityHeader({ city }: { city: any }) {
   return (
     <Box width='auto' height='auto' bottom='50%'>
-      <Text as='p' size='4'>
+      <Text as='p' size='3'>
         Workspaces in
       </Text>
-      <Heading data-testid='city-name' as='h1' size='8'>
-        {city[0].name}
+      <Heading data-testid='city-name' as='h1' size='9'>
+        {city[0].name},
       </Heading>
-      <Text as='p' size='6'>
+      <Text as='p' size='7'>
         {city[0].country}
       </Text>
     </Box>
